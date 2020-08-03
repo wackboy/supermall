55b31a294ebd873972f5b98b63fb1440e054543d
Suiyuan <15955176942@163.com>
2020-08-03 09:30:13 +0800

    更新

---
+++ src/network/request.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+
+
+
+export function request(config) { 
+        //1. 创建axios的实例
+        const instance = axios.create({
+            baseURL: 'http://123.207.32.32:8000',
+            timeout: 5000
+        })  
+
+        //2. axios的拦截器
+        //2.1
+        instance.interceptors.request.use(aaa => {
+            return aaa
+        }, err => {
+            // console.log(err);
+        })
+
+        //2.2 响应拦截
+        instance.interceptors.response.use(res => {
+            // console.log(res);
+            return res.data
+        }, err => {
+            console.log(err);
+        })
+
+        
+        //3. 发送真正的网络请求
+        return instance(config)
+
+ }
